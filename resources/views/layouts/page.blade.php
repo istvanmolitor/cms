516818878667187419aec34064d200366a8bcc92
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-03-03 14:08:32 +0100

    themes

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,11 +1,11 @@
 @extends('cms::layouts.base')
 
 @section('body')
-    @include('cms::layouts.partials.header')
+    @themeInclude('cms::layouts.partials.header')
 
     <main class="flex-grow">
         @yield('page')
     </main>
 
-    @include('cms::layouts.partials.footer')
+    @themeInclude('cms::layouts.partials.footer')
 @endsection
