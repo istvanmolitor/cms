17752aba4cb700db572e2fc84827a7d87019e438
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-03-04 10:15:23 +0100

    video bug

--- resources/views/components/content-elements/video.blade.php
+++ resources/views/components/content-elements/video.blade.php
@@ -1,8 +1,5 @@
 @php
-    $data = json_decode($content, true) ?? [];
-    $url = $data['url'] ?? $content;
-    $width = $data['width'] ?? '300px';
-    $height = $data['height'] ?? '450px';
+    $url = $settings['url'];
 
     // YouTube embed URL conversion
     if (preg_match('/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/', $url, $matches)) {
@@ -10,7 +7,7 @@
     }
 @endphp
 <div class="content-element content-element-video my-8">
-    <div class="relative overflow-hidden rounded-lg shadow-md" style="width: {{ $width }}; height: {{ $height }};">
+    <div class="relative overflow-hidden rounded-lg shadow-md" style="width: {{ $settings['width'] }}; height: {{ $settings['height'] }};">
         <iframe src="{{ $url }}"
                 frameborder="0"
                 allowfullscreen
