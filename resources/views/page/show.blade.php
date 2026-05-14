a8e70895e9ce2db873692d7244f7292693622247
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-05-14 17:56:19 +0200

    show

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -5,7 +5,6 @@
         <h1 class="text-3xl lg:text-4xl font-bold text-gray-900 mb-6">
             {{ $page->title }}
         </h1>
-
         <div class="prose prose-lg max-w-none">
             <x-cms-content :content="$page->content" />
         </div>
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,7 @@ use Molitor\Cms\Http\Controllers\HomepageController;
 use Molitor\Cms\Http\Controllers\PageController;
 use Molitor\Cms\Http\Controllers\PageGroupController;
 
-Route::get('/', [HomepageController::class, 'index'])->name('home');
-Route::get('/page/{slug}', [PageController::class, 'show'])->name('page.show');
-Route::get('/page-group/{slug}', [PageGroupController::class, 'show'])->name('page-group.show');
+Route::get('/', [HomepageController::class, 'index'])->name('cms.homepage');
+Route::get('/page', [PageController::class, 'index'])->name('cms.page.index');
+Route::get('/page/{slug}', [PageController::class, 'show'])->name('cms.page.show');
+Route::get('/page-group/{slug}', [PageGroupController::class, 'show'])->name('cms.page-group.show');
