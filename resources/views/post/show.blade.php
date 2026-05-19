2c41203f92c7384d4a6881df926729d3a11addd9
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-05-19 11:05:54 +0200

    show

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,9 +2,24 @@
 
 @section('content')
     <article class="bg-white rounded-lg shadow-sm p-6 lg:p-8">
-        <h1 class="text-3xl lg:text-4xl font-bold text-gray-900 mb-6">
+         <h1 class="text-3xl lg:text-4xl font-bold text-gray-900 mb-6">
             {{ $post->title }}
         </h1>
+        @if($post->main_image_url)
+            <img
+                src="{{ $post->main_image_url }}"
+                alt="{{ $post->title }}"
+                class="w-full h-auto rounded-lg mb-6 object-cover"
+            >
+        @endif
+
+
+        @if($post->lead)
+            <p class="mb-8 px-5 py-4 rounded-lg border-l-4 border-blue-500 bg-blue-50 text-lg lg:text-xl font-medium text-gray-900">
+                {{ $post->lead }}
+            </p>
+        @endif
+
         <div class="prose prose-lg max-w-none">
             <x-cms-content :content="$post->content" />
         </div>
