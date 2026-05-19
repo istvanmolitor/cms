7b2b3af666316a2c21fe85713a30162263f518ba
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-05-19 10:55:56 +0200

    bug

--- resources/views/components/content-elements/list.blade.php
+++ resources/views/components/content-elements/list.blade.php
@@ -1,6 +1,13 @@
-<div class="content-element content-element-list mb-6">
-    <div class="text-gray-700 space-y-2">
-        {!! $content !!}
-    </div>
-</div>
+@php
+    $items = $settings['items'] ?? [];
+@endphp
 
+<div class="content-element content-element-list mb-6">
+    @if(!empty($items))
+        <ul class="list-disc pl-6 space-y-2 text-gray-700">
+            @foreach($items as $item)
+                <li>{{ $item }}</li>
+            @endforeach
+        </ul>
+    @endif
+</div>
--- resources/views/components/content-elements/quote.blade.php
+++ resources/views/components/content-elements/quote.blade.php
@@ -1,6 +1,9 @@
+@php
+    $text = $settings['text'] ?? '';
+@endphp
+
 <div class="content-element content-element-quote my-8">
     <blockquote class="border-l-4 border-gray-300 pl-6 py-4 italic text-gray-700 text-lg">
-        {!! $content !!}
+        {{ $text }}
     </blockquote>
 </div>
-
