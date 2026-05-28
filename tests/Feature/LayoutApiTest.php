834609ee27caf0643fe9c3685537f5047151cd38
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-05-28 20:01:26 +0200

    test

---
+++ tests/Feature/LayoutApiTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class LayoutApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_can_list_layouts(): void
+    {
+        $response = $this->getJson('/api/cms/layouts');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'container' => [
+                        'name',
+                        'template',
+                    ],
+                    'full-width',
+                ],
+            ]);
+
+        $this->assertArrayHasKey('container', $response->json('data'));
+        $this->assertIsString($response->json('data.container.name'));
+        $this->assertEquals('theme::layouts.container', $response->json('data.container.template'));
+    }
+}
+
