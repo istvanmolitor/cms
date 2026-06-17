7bc1da8966f71e666982ca44d2e43436da4347d1
Molitor István <istvanmolitor@gmail.com>
2026-06-17 14:50:15 +0200

    routing

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,12 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Molitor\Cms\Http\Controllers\AuthorController;
-use Molitor\Cms\Http\Controllers\HomepageController;
 use Molitor\Cms\Http\Controllers\PageController;
 use Molitor\Cms\Http\Controllers\PostController;
 use Molitor\Cms\Http\Controllers\PostGroupController;
 
-Route::get('/', [HomepageController::class, 'index'])->name('cms.homepage');
 Route::get('/page', [PageController::class, 'index'])->name('cms.page.index');
 Route::get('/page/{slug}', [PageController::class, 'show'])->name('cms.page.show');
 Route::get('/post', [PostController::class, 'index'])->name('cms.post.index');
