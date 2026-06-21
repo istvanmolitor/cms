6cd10fa12023d4531d092cf79c7abed29325cf94
Molitor István <istvanmolitor@gmail.com>
2026-06-21 13:23:26 +0200

    author images

--- src/Http/Requests/Author/StoreAuthorRequest.php
+++ src/Http/Requests/Author/StoreAuthorRequest.php
@@ -11,7 +11,7 @@ class StoreAuthorRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return Gate::allows('acl', 'cms');
+        return Gate::allows('acl', 'cms_author');
     }
 
     /**
--- src/Http/Requests/Author/UpdateAuthorRequest.php
+++ src/Http/Requests/Author/UpdateAuthorRequest.php
@@ -11,7 +11,7 @@ class UpdateAuthorRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return Gate::allows('acl', 'cms');
+        return Gate::allows('acl', 'cms_author');
     }
 
     /**
