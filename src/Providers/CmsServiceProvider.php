3fd4ce5c98946055b49d06c42b788df507d01289
Molitor István <istvanmolitor@gmail.com>
2026-06-27 17:52:32 +0200

    search

--- src/Providers/CmsServiceProvider.php
+++ src/Providers/CmsServiceProvider.php
@@ -41,6 +41,8 @@ use Molitor\Cms\Repositories\PostRepositoryInterface;
 use Molitor\Cms\Services\CmsComponentRegistry;
 use Molitor\Cms\Services\CmsSettingForm;
 use Molitor\Cms\Services\ContentHandler;
+use Molitor\Search\Searchers\LikeSearcher;
+use Molitor\Search\Services\SearchRegistry;
 use Molitor\Setting\Services\SettingHandler;
 
 class CmsServiceProvider extends ServiceProvider
@@ -67,6 +69,11 @@ class CmsServiceProvider extends ServiceProvider
         Blade::componentNamespace('Molitor\\Cms\\View\\Components', 'cms');
 
         $this->app->make(SettingHandler::class)->registerSettingForm(CmsSettingForm::class);
+
+        $this->app->make(SearchRegistry::class)->register(
+            Post::class,
+            new LikeSearcher(Post::class, ['title', 'lead']),
+        );
 
         $contentObserver = new ContentObserver;
         Page::observe($contentObserver);
