978f6e36ae8af2069d8d19011e66bb8c0bf105f5
Molitor István <istvanmolitor@gmail.com>
2026-07-02 21:35:51 +0200

    sidebar

--- src/Http/Controllers/HomepageController.php
+++ src/Http/Controllers/HomepageController.php
@@ -6,17 +6,20 @@ namespace Molitor\Cms\Http\Controllers;
 
 use Illuminate\Http\Response;
 use Illuminate\View\View;
+use Molitor\Cms\Services\CmsSettingForm;
 use Molitor\Theme\Services\LayoutService;
 
 class HomepageController
 {
     public function __construct(
-        private LayoutService $layoutService
+        private LayoutService $layoutService,
+        private CmsSettingForm $cmsSettingForm
     ) {}
 
     public function index(): View|Response
     {
-        $layout = $this->layoutService->getLayoutTemplate();
+        $layoutName = $this->cmsSettingForm->get('homepage_layout');
+        $layout = $this->layoutService->getLayoutTemplate($layoutName);
 
         return template('cms::homepage.index', [
             'layout' => $layout,
